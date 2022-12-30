6c87597cefe5bf46842d9b4d68b5b4c73f6d22ec
Hassan <hassan.imhah@outlook.fr>
2022-12-30 15:37:10 +0100

    Alter db

--- db/AlterDb.sql
+++ db/AlterDb.sql
@@ -1,3 +1,40 @@
+CREATE TABLE [Audit] (
+[AuditId] INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
+[tableName] VARCHAR(100)  NULL,
+[tableColumn] VARCHAR(100)  NULL,
+[kayName] VARCHAR(100)  NULL,
+[kayValue] VARCHAR(100)  NULL,
+[oldValue] VARCHAR(100)  NULL,
+[newValue] VARCHAR(100)  NULL,
+[updateDate] DATETIME  NULL);
+
+
+Alter Table Orders Add Column TotalPrice FLOAT;
+Alter Table Flights Add Column TicketPriceFirst FLOAT;
+Alter Table Flights Add Column TicketPriceBusiness FLOAT;
+
+Update Flights 
+Set		TicketPriceFirst = TicketPrice * 1.2
+		, TicketPriceBusiness = TicketPrice * 1.3
+;
+
+
+Update	 Orders
+SET		 TotalPrice = (Select TicketPrice*Orders.TicketsOrdered From Flights f where f.FlightNumber = Orders.FlightNumber)
+WHERE	 Class = 3
+;
+
+Update	 Orders
+SET		 TotalPrice = (Select TicketPriceFirst*Orders.TicketsOrdered From Flights f where f.FlightNumber = Orders.FlightNumber)
+WHERE	 Class = 2
+;
+
+Update	 Orders
+SET		 TotalPrice = (Select TicketPriceBusiness*Orders.TicketsOrdered From Flights f where f.FlightNumber = Orders.FlightNumber)
+WHERE	 Class = 1
+;
+
+
 Alter Table Users Add Column Profil NVARCHAR(10);
 
 Update Users 
@@ -9,12 +46,5 @@ Where   username = 'john';
 
 Select * from Users;
 
-Alter Table Flights Add Column TicketPriceFirst FLOAT;
-Alter Table Flights Add Column TicketPriceBusiness FLOAT;
 
-Update Flights 
-Set		TicketPriceFirst = TicketPrice * 1.2
-		, TicketPriceBusiness = TicketPrice * 1.3
-;
-
-Select * from Flights;
+Select * from Orders o ;
