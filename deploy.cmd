d9f4a98421022c10fdeeec6af5f9d2e1538ea263
Hassan <h.imhah@groupeonepoint.com>
2024-10-19 22:25:41 +0200

    Exécution avec Python 3.11

---
+++ deploy.cmd
@@ -0,0 +1,9 @@
+set dest=c:\temp\PyFlightApi
+mkdir %dest%
+copy flightapi.ini %dest%
+copy flights_db.py %dest%
+copy logging.conf %dest%
+copy flight_rest.py %dest%
+copy config.py %dest%
+mkdir %dest%\db
+copy db\Flights.s3db %dest%\db
